22851fa1414d30d554840768cd21ce45cfbae7e2
Andresxsx <and97436@gmail.com>
2026-06-10 00:25:06 -0500

    Corregir assets de Filament en Render

--- start.sh
+++ start.sh
@@ -11,13 +11,15 @@ mkdir -p storage/framework/sessions
 mkdir -p storage/framework/views
 mkdir -p bootstrap/cache
 
-chown -R www-data:www-data storage bootstrap/cache
-chmod -R 775 storage bootstrap/cache
+chown -R www-data:www-data storage bootstrap/cache public
+chmod -R 775 storage bootstrap/cache public
 
 php artisan optimize:clear || true
 
 php artisan migrate --force
 php artisan db:seed --force || true
+
+php artisan filament:assets || true
 
 php artisan config:cache
 php artisan view:cache
